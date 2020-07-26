c321e7c67f6f6ec040bbae85486c6e2d2a470e83
Jatin Gupta <guptajatin@outlook.com>
2020-07-27 09:09:17 +1000

    init

---
+++ COMP9313_project_2/submission.py
@@ -0,0 +1,8 @@
+def base_features_gen_pipeline(input_descript_col="descript", input_category_col="category", output_feature_col="features", output_label_col="label"):
+    pass
+
+def gen_meta_features(training_df, nb_0, nb_1, nb_2, svm_0, svm_1, svm_2):
+    pass
+
+def test_prediction(test_df, base_features_pipeline_model, gen_base_pred_pipeline_model, gen_meta_feature_pipeline_model, meta_classifier):
+    pass
